450f1490a2d047666cd00288953d09fb2849fda0
1amKhush <khushvendras99@gmail.com>
2025-01-27 05:25:03 +0530

    Added navigation links to sections on the page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
                             <a href="#about">About</a>
                         </li>
                         <li>
-                            <a href="#experience">Experience</a>
+                            <a href="#projects">Experience</a>
                         </li>
                         <li>
                             <a href="#projects">Projects</a>
