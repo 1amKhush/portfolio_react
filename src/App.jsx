eeeb4e5dc80696516fbacfb53e8616c5c5ca6ed0
1amKhush <khushvendras99@gmail.com>
2025-02-16 04:07:50 +0530

    Fixed spelling error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import styles from './App.module.css'
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
