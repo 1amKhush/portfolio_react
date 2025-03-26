36ae057b6b1889440181ae19a4c5a7795a72277a
1amKhush <khushvendras99@gmail.com>
2025-03-26 16:40:29 +0530

    Improveed project card styling

--- assets/projects/projectImages.js
+++ assets/projects/projectImages.js
@@ -1,9 +1,11 @@
 import pacman from './pacman.png';
 import posterverse from './posterverse.jpg';
 import volunteersiitj from './volunteersiitj.png';
+import webwallet from './WebWallet.png'
 
 export default {
   pacman,
   posterverse,
   volunteersiitj,
+  webwallet
 };
